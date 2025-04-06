3b8c51c6275f61f6e47c46d6aa9e69c01a0b6bac
OToL <barchange@yahoo.fr>
2025-04-06 22:29:29 +0200

    Display simple raylib window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
-#include <cstdio>
+#include <raylib.h>
 
 int main() {
 
-    printf("Hello world %d\n", 3);
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Raytracing");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("Hello Raylib", 20, 20, 50, BLACK);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 
     return 0;
 }
